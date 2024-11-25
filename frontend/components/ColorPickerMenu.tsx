4e7d142780761ed2fa68abc8e91d514952b566cf
Mariam <mariam.ashraf.attia.zaki@gmail.com>
2024-11-25 22:03:33 +0200

    feat:set Background and font colors for presentation page using callback function

--- frontend/components/ColorPickerMenu.tsx
+++ frontend/components/ColorPickerMenu.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { View } from "react-native";
-import useHymnosState from "../global";
+
 // https://stackoverflow.com/questions/71818458/why-wont-tailwind-find-my-dynamic-class
 
 function ColorRow({ color, onSelectColor }: { color: string; onSelectColor: (color: string) => void }) {
@@ -18,81 +18,20 @@ function ColorRow({ color, onSelectColor }: { color: string; onSelectColor: (col
   );
 }
 
-export default function ColorPickerMenu({ type }: { type: "background" | "font" }) {
-  const { setPresentationSettings } = useHymnosState();
+export default function ColorPickerMenu({ onColorSelect, }: { onColorSelect: (color: string) => void; }) {
+  
 
-  const handleColorSelect = (color: string) => {
-    //console.log(`Selected color: ${color}`);
-    if (type === "background") {
-      setPresentationSettings({ backgroundColor: color });
-    } else if (type === "font") {
-      setPresentationSettings({ fontColor: color });
-    }
-  };
 
   return (
     <View className="p-3 bg-slate-200 rounded-md space-y-3">
-      <ColorRow color="blue" onSelectColor={handleColorSelect} />
-      <ColorRow color="cyan" onSelectColor={handleColorSelect} />
-      <ColorRow color="green" onSelectColor={handleColorSelect} />
-      <ColorRow color="red" onSelectColor={handleColorSelect} />
-      <ColorRow color="yellow" onSelectColor={handleColorSelect} />
-      <ColorRow color="slate" onSelectColor={handleColorSelect} />
-      <ColorRow color="gray" onSelectColor={handleColorSelect} />
+      <ColorRow color="blue" onSelectColor={onColorSelect} />
+      <ColorRow color="cyan" onSelectColor={onColorSelect} />
+      <ColorRow color="green" onSelectColor={onColorSelect} />
+      <ColorRow color="red" onSelectColor={onColorSelect} />
+      <ColorRow color="yellow" onSelectColor={onColorSelect} />
+      <ColorRow color="slate" onSelectColor={onColorSelect} />
+      <ColorRow color="gray" onSelectColor={onColorSelect} />
     </View>
   );
 }
 
-
-/*
-function ColorRow({ color, onSelectColor }: { color: string; onSelectColor: (color: string) => void }) {
-    return(
-    <View className="flex flex-row gap-x-1">
-      <Pressable
-        className={`bg-${color}-100 w-5 h-5 rounded-md border-gray-700 border hover:scale-110 transition duration-100 ease-in-out`}
-        onPress={() => onSelectColor(`${color}-100`)}
-      />
-      <Pressable
-        className={`bg-${color}-300 w-5 h-5 rounded-md border-gray-700 border hover:scale-110 transition duration-100 ease-in-out`}
-        onPress={() => onSelectColor(`${color}-300`)}
-      />
-      <Pressable
-        className={`bg-${color}-500 w-5 h-5 rounded-md border-gray-700 border hover:scale-110 transition duration-100 ease-in-out`}
-        onPress={() => onSelectColor(`${color}-500`)}
-      />
-      <Pressable
-        className={`bg-${color}-700 w-5 h-5 rounded-md border-gray-700 border hover:scale-110 transition duration-100 ease-in-out`}
-        onPress={() => onSelectColor(`${color}-700`)}
-      />
-      <Pressable
-        className={`bg-${color}-900 w-5 h-5 rounded-md border-gray-700 border hover:scale-110 transition duration-100 ease-in-out`}
-        onPress={() => onSelectColor(`${color}-900`)}
-      />
-    </View>
-  );
-}
-
-export default function ColorPickerMenu({ type }: { type: "background" | "font" }) {
-  const { setPresentationSettings } = useHymnosState();
-
-  const handleColorSelect = (color: string) => {
-    // Based on the type ('background' or 'font'), update the respective color
-    if (type === "background") {
-      setPresentationSettings({ backgroundColor: color });
-    } else if (type === "font") {
-      setPresentationSettings({ fontColor: color });
-    }
-  };
-
-  return (
-    <View className="p-3 bg-slate-200 rounded-md space-y-3">
-      <ColorRow color="blue" onSelectColor={handleColorSelect} />
-      <ColorRow color="cyan" onSelectColor={handleColorSelect} />
-      <ColorRow color="green" onSelectColor={handleColorSelect} />
-      <ColorRow color="red" onSelectColor={handleColorSelect} />
-      <ColorRow color="yellow" onSelectColor={handleColorSelect} />
-      <ColorRow color="slate" onSelectColor={handleColorSelect} />
-      <ColorRow color="gray" onSelectColor={handleColorSelect} />
-    </View>
-  );
-}*/
