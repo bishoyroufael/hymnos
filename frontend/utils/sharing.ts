927b19d305ab37735ebc0bbdbbb749ea07f2deb0
bishoyroufael <bishoy.roufael@dnv.com>
2025-05-31 20:30:26 +0200

    feat(frontend): remove console log, and add toast to presentation screen

--- frontend/utils/sharing.ts
+++ frontend/utils/sharing.ts
@@ -1,3 +1,4 @@
+import { emitError } from "@utils/notification";
 import { Share } from "react-native";
 
 export const shareText = async (message: string, url: string) => {
@@ -7,6 +8,7 @@ export const shareText = async (message: string, url: string) => {
       url: url,
     });
   } catch (error) {
+    emitError("لا يمكن المشاركه علي هذا المتصفح");
     console.error("Error sharing:", error);
   }
 };
